5b2b9686f467dff27530c131714b5845586255e3
Iram Khan <149258382+iram-3110@users.noreply.github.com>
2024-10-30 19:18:08 +0530

    Solution.java
    LeetCode Array ques number 53

---
+++ Arr7.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/maximum-subarray/
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n= nums.length;
+        int sum= nums[0], max=nums[0];
+
+        for(int i=1; i<n; i++){
+            sum= Math.max(nums[i], sum+nums[i]);
+            max= Math.max(sum,max);
+        }
+        return max;
+    }
+}
